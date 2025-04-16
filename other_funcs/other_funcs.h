4f920f53ecbf980b6f8c9438818752e840a14c84
JusticeIL <galru3@mta.ac.il>
2025-04-16 10:02:06 +0300

    Added all needed functions. The function for the reload database doesn't work properly.

---
+++ other_funcs/other_funcs.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <list>
+#include <string>
+
+struct Block{
+    std::string hash;
+    int height;
+    long long total;
+    std::string time;
+    std::string relayed_by;
+    std::string prev_block;
+};
+
+//Functions Declarations
+bool open_new_csv_file(std::string filename);
+void print_csv_menu_to_file(std::string filename);
+void printValuesToCSVFile(std::string filename, std::list<Block> lst);
+void reloadDatabase();
+void userMenu(std::list<Block>& blocks, std::string filename, std::string out);
+void printUserMenu();
+void performFunc(int choice, const std::list<Block>& blocks, std::string filename);
