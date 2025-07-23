16d20ed5e7bd603e5cc1ecd990935ecbb7e14a84
mayageva2 <mayageva7@gmail.com>
2025-07-23 12:09:34 +0300

    adding encrypter main and deleting thread related code - needs testing

--- decryption/main.c
+++ decryption/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <pthread.h>
 #include <string.h>
 #include <mta_crypt.h> 
 #include "shared.h"
-#include "encrypt.h"
-#include "decrypt.h"
+#include "encrypt_funcs.h"
 #include "config.h"
 
 //Global variables
@@ -16,9 +14,6 @@ SharedData shared = {
     .decrypted = false,
     .guess_pending = false,
     .guesser_id = -1,
-    .mutex = PTHREAD_MUTEX_INITIALIZER,
-    .cond = PTHREAD_COND_INITIALIZER,
-    .guess_cond = PTHREAD_COND_INITIALIZER 
 };
 int password_length;
 int num_decrypters;
@@ -75,18 +70,6 @@ int main(int argc, char *argv[])
        fprintf(stderr, "Password length must be > 0, divisible by 8, and <= %d.\n", MAX_PASSWORD_LENGTH);
        exit(EXIT_FAILURE);
    }
-
-   //Decrypter threads creation
-   pthread_t* decrypter_threads = create_decrypter_threads(num_decrypters); //Call decryptors
-
-   pthread_t encrypter_thread;
-   if (pthread_create(&encrypter_thread, NULL, encrypter, NULL) != 0) //Added memory allocation check
-   {
-       printf("Failed allocate memory for encryptor thread");
-       exit(1);
-   }
-
-   pthread_join(encrypter_thread, NULL);
 
    return 0;
 }
